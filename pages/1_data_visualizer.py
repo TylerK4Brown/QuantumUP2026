c1b58c70aeb9e245e0c23dabd09931f14cf9dae3
Gjin <74433905+gjinrexhaj@users.noreply.github.com>
2026-04-04 11:48:00 -0400

    begin work on visualizer

--- pages/1_data_visualizer.py
+++ pages/1_data_visualizer.py
@@ -1,4 +1,18 @@
 import streamlit as st
-
+import pandas as pd
 
 st.write("# 1")
+
+df_t1 = pd.read_csv("data/t1_quantum_scrubbed.csv")
+df_t2 = pd.read_csv("data/t2_quantum_scrubbed.csv")
+df_bb = pd.read_csv("data/BB_quantum_scrubbed.csv")
+
+bg_combined = pd.DataFrame({
+    't1': df_t1['severity'].value_counts(),
+    't2': df_t2['severity'].value_counts(),
+    'bb': df_bb['Severity'].value_counts()
+})
+
+st.write(bg_combined.head())
+
+st.bar_chart(bg_combined.head(20000))
