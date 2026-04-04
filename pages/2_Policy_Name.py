1cdf81c2779963dbc0fdb915633fbfa14cab57ba
Jawnpog <kniimare@gmail.com>
2026-04-04 14:50:12 -0400

    Updated page file order and created different keyword search page files

---
+++ pages/2_Policy_Name.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Keyword Search")
+
+
+st.text_input("Policy Name", placeholder='Enter a keyword')
+
---
+++ pages/4_Name.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+st.title("Keyword Search")
+
+
+st.text_input("Name", placeholder='Enter a keyword')
