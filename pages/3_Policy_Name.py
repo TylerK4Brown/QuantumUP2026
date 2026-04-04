11602bc439aea2b02f082b289f577c54b6a271c8
Jawnpog <kniimare@gmail.com>
2026-04-04 15:50:54 -0400

    Created dropdown that allows the user to select which column to check for a keyword within returning the entire row

--- pages/3_Policy_Name.py
+++ pages/3_Policy_Name.py
@@ -1,8 +1,29 @@
 import streamlit as st
-import pandas as pd
 
 st.title("Keyword Search")
 
+option = st.selectbox(
+    'Select a field:',
+    (
+        'Resource Name',
+        'Cloud Provider ID',
+        'Native Type', 'Tags',
+        'Policy Name', 'Severity',
+        'Result',
+        'Compliance Check Name',
+        'Category',
+        'Remediation Steps',
+        'Assessed At',
+        'Subscription Name',
+        'Subscription Provider ID'
+    ))
 
-st.text_input("Policy Name", placeholder='Enter a keyword')
+input = st.text_input("Search", placeholder='Enter a keyword')
+
+if input:
+    valid_rows = st.session_state.df_bb[st.session_state.df_bb[option].str.contains(input, na=False, case=False)]
+else:
+    valid_rows = st.session_state.df_bb
+
+st.write(valid_rows.reset_index(drop=True))
 
--- pages/5_Name.py
+++ pages/5_Name.py
@@ -1,7 +1,3 @@
 import streamlit as st
-import pandas as pd
 
-st.title("Keyword Search")
-
-
-st.text_input("Name", placeholder='Enter a keyword')
+st.title("TODO")
