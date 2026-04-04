99d8223439227a0d9fb1ddae49b54724371712e3
Jawnpog <kniimare@gmail.com>
2026-04-04 14:34:19 -0400

    Updated home.py to use session state for the three dataframe data sets and updated Data Visualizer accordingly

--- home.py
+++ home.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title="Home",
 )
 
 st.write("# Welcome!")
+
+# Initialization for session state
+if 'df_t1' not in st.session_state:
+    st.session_state['df_t1'] = pd.read_csv("data/t1_quantum_scrubbed.csv")
+
+if 'df_t2' not in st.session_state:
+    st.session_state['df_t2'] = pd.read_csv("data/t2_quantum_scrubbed.csv")
+
+if 'df_bb' not in st.session_state:
+    st.session_state['df_bb'] = pd.read_csv("data/BB_quantum_scrubbed.csv")
--- pages/1_data_visualizer.py
+++ pages/1_data_visualizer.py
@@ -3,14 +3,10 @@ import pandas as pd
 
 st.write("# 1")
 
-df_t1 = pd.read_csv("data/t1_quantum_scrubbed.csv")
-df_t2 = pd.read_csv("data/t2_quantum_scrubbed.csv")
-df_bb = pd.read_csv("data/BB_quantum_scrubbed.csv")
-
 bg_combined = pd.DataFrame({
-    't1': df_t1['severity'].value_counts(),
-    't2': df_t2['severity'].value_counts(),
-    'bb': df_bb['Severity'].value_counts()
+    't1': st.session_state.df_t1['severity'].value_counts(),
+    't2': st.session_state.df_t2['severity'].value_counts(),
+    'bb': st.session_state.df_bb['Severity'].value_counts()
 })
 
 st.write(bg_combined.head())
