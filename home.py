1ca8cf7922f70d41441dd5a294c9b96094585c92
Jawnpog <kniimare@gmail.com>
2026-04-04 15:00:49 -0400

    Updated page file names to reorder and added dropdowns to navigation

--- home.py
+++ home.py
@@ -16,3 +16,25 @@ if 'df_t2' not in st.session_state:
 
 if 'df_bb' not in st.session_state:
     st.session_state['df_bb'] = pd.read_csv("data/BB_quantum_scrubbed.csv")
+
+# Navigation Logic and Order
+pages = {
+    "Data": [
+        st.Page("2_Data_Visualizer.py", title="Visualizer")
+    ],
+    "Keyword Search": [
+        st.Page("3_Policy_Name.py", title="Policy Name"),
+        st.Page("4_Resource_Name.py", title="Resource Name"),
+        st.Page("5_Name.py", title="Name")
+    ],
+    "Risk Assessment": [
+        st.Page("6_Vulnerability_Assessment.py", title="Rationale"),
+        st.Page("7_Analysis.py", title="Analysis"),
+    ],
+    "Results": [
+        st.Page("8_Solution.py", title="Solution")
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
