911f4072e8a56816f7379b0edfd443bf17b7c399
Gjin <74433905+gjinrexhaj@users.noreply.github.com>
2026-04-05 20:36:24 -0400

    revert to nominal data

--- pages/2_Data_Visualizer.py
+++ pages/2_Data_Visualizer.py
@@ -27,10 +27,10 @@ t1_df = st.session_state['df_t1']
 bb_df = st.session_state['df_bb']
 
 # Convert nominal values to numeric values
-severity_mapping = {"Info": 1, "Low": 2, "Medium": 3, "High": 4, "Critical": 5}
-t1_df["severity_numerical"] = t1_df["severity"].map(severity_mapping)
-state_mapping = {"NEW": 1, "ACTIVE": 2, "RESURFACED": 3}
-t1_df["state_numerical"] = t1_df["state"].map(state_mapping)
+#severity_mapping = {"Info": 1, "Low": 2, "Medium": 3, "High": 4, "Critical": 5}
+#t1_df["severity_numerical"] = t1_df["severity"].map(severity_mapping)
+#state_mapping = {"NEW": 1, "ACTIVE": 2, "RESURFACED": 3}
+#t1_df["state_numerical"] = t1_df["state"].map(state_mapping)
 
 
 # Present filtering options for t1 dataframe
@@ -43,7 +43,7 @@ options = st.multiselect(
 min_num = 0
 max_num = 100
 
-
+st.write(f"Dataset size: [0 : {t1_df.index.stop}]")
 col1, col2 = st.columns(2)
 with col1:
     min_num = st.number_input("Min", min_value=0, max_value=len(t1_df.index-1), value=0)
@@ -62,9 +62,9 @@ if options:
         st.bar_chart(df_port.iloc[min_num: max_num])
 
     if "Severity" in options:
-        df_severity = pd.concat([df_severity, t1_df["severity_numerical"]], axis=1)
+        df_severity = pd.concat([df_severity, t1_df["severity"]], axis=1)
         st.bar_chart(df_severity.iloc[min_num: max_num])
 
     if "State" in options:
-        df_state = pd.concat([df_state, t1_df["state_numerical"]], axis=1)
+        df_state = pd.concat([df_state, t1_df["state"]], axis=1)
         st.bar_chart(df_state.iloc[min_num: max_num])
